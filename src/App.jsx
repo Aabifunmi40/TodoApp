010d17f66e64e5b4c42b2e32f83ab2b2d293e08f
Alabi Oluwafunmilayo <kkoredesemi@gmail.com>
2025-09-05 00:08:43 +0100

    Add AuthContext and protect TaskPage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
-import Taskpage from "./pages/Taskpage";
+import Taskpage from "./pages/Taskpage"; // Make sure the casing matches the file
 import Header from "./components/Header";
 import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -13,7 +14,16 @@ function App() {
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/" element={<Taskpage />} />
+          
+          {/* Protected route */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Taskpage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </AuthProvider>
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+// src/components/ProtectedRoute.jsx
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    // Redirect to sign-in page if not logged in
+    return <Navigate to="/signin" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
